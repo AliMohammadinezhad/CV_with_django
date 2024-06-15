cc3adb4324a8a34fb97581af465b3230a4df6b4a
Ali Mohammadinezhad <alimn1382@hotmail.com>
2024-06-15 22:03:53 +0330

    add template

--- config/settings.py
+++ config/settings.py
@@ -115,13 +115,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = 'static/'
-
-STATICFILES_DIR = [
-    BASE_DIR / 'statics'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    BASE_DIR / "statics",
 ]
-
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = 'media/'
