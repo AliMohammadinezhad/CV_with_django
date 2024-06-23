343373c0deddf7d79f3840be0e4c84e520770fd4
Ali Mohammadinezhad <alimn1382@hotmail.com>
2024-06-23 13:15:36 +0330

    add context data for rendering

--- cv/views.py
+++ cv/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'cv/index.html')
+    context = {
+        "name": "Ali",
+        "last_name": "Mohammadinezhad",
+        "phone": "+98 935 213 3383",
+        "email": "alimn1382@gmail.com",
+        "address": "Tabriz, East Azarbaijan, Iran"
+        }
+    return render(request, 'cv/index.html', context)
